c3e23b3ad9f10f6c6d735ec74ff0cdeb1de04520
Raph Gallardo <draphick@gmail.com>
2020-02-03 18:58:24 -0800

    initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# pyMouseClick
+
+``` bash
+pip install -r requirements.txt
+python ./mouseclick.py
+```
+
+Press CTRL+C to cancel/stop the script.  
---
+++ mouseclick.py
@@ -0,0 +1,9 @@
+import pyautogui, sys
+import time
+
+# ctrl+c to stop/exit script
+while True:
+    # sleep 1 second
+    time.sleep(1)
+    # clicking mouse
+    pyautogui.click()
